de10817523e08efc44187c4340a4290820c3d53d
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-08-03 11:05:25 +0200

    Basic math

--- main.cs
+++ main.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class Class1
 {
 	public Class1()
 	{
+
+        static double Plus(double a, double b)
+        {
+            double value = 0;
+            value = a + b;
+            return value;
+        }
+
+        static double Minus(double b, double a)
+        {
+            double value = 0;
+            value = a - b;
+            return value;
+        }
+
+        static double Gange(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Divider(double a, double b)
+        {
+            return a / b;
+        }
+
 	}
 }
 
